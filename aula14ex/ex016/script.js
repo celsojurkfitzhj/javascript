1598e2d02c920a1b50e543d8d05fe811157daca9
celsojurkfitzhj <105890184+celsojurkfitzhj@users.noreply.github.com>
2022-05-26 17:19:28 -0300

    up

--- aula14ex/ex016/script.js
+++ aula14ex/ex016/script.js
@@ -8,17 +8,22 @@ function contar(){
     var fim = Number(txtfim.value)
     var passo = Number(txtpasso.value)
     res.innerHTML = (' ')
-    if (inicio == " " || fim == " " || passo == " "){
+    if (txtinicio.value.length == 0 || txtfim.value.length == 0 || txtpasso.value.length == 0 ){
         res.innerHTML = ("Impossivel de contar")
     }else{
-        if (passo == 0){
+        if (passo <= 0){
             window.alert('Atenção valor do passo invalido, logo foi mudado para 1')
             var passo = 1
+        } 
+        res.innerHTML = ('Contando: <br>')
+        if (inicio > fim){
+            for (var c = inicio; c >= fim ; c -= passo)
+
+            res.innerHTML += (` ${c}  \u{1F449}`)
+        }else{
+            for (var c = inicio; c <=fim ; c += passo)
+            res.innerHTML += (` ${c}  \u{1F449}`)
         }
-        res.innerHTML = ("Contando:<br>")
-        for(var c = inicio; c <= fim ;c = c + passo){
-            
-            res.innerHTML += (c + " ")
-        }
+        res.innerHTML += `\u{1F3C1}`
     }
 }
--- aula14ex/ex017/script.js
+++ aula14ex/ex017/script.js
@@ -1,17 +1,18 @@
 function tabuada(){
     var txtnumber = window.document.getElementById('txtnumber')
-    var res = window.document.getElementById('res')
+    var tab = window.document.getElementById('seltab')
 
-    var number = Number(txtnumber.value)
-    var restab = 0
-
-    if (number == ' '){
+    if (txtnumber.value.length == 0){
         window.alert('Por favor insira algum número para continuar')
     }else{
+        var number = Number(txtnumber.value)
+        var restab = 0
+        tab.innerHTML = ''
         for(var c = 1;c <= 10; c++ ){
             restab = c * number 
-            
-            res.innerHTML += (`${number} X ${c} = ${restab}`) 
+            var item = document.createElement('option')
+            item.text = `${number} X ${c} = ${restab}`
+            tab.appendChild(item)
         }
     }
 }
