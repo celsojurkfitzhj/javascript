4a1fc183c1bbf2f1ae65d20d863053b8a2099c81
celsojurkfitzhj <105890184+celsojurkfitzhj@users.noreply.github.com>
2022-05-26 15:56:52 -0300

    up

--- aula14ex/ex017/script.js
+++ aula14ex/ex017/script.js
@@ -1,3 +1,17 @@
 function tabuada(){
-    
+    var txtnumber = window.document.getElementById('txtnumber')
+    var res = window.document.getElementById('res')
+
+    var number = Number(txtnumber.value)
+    var restab = 0
+
+    if (number == ' '){
+        window.alert('Por favor insira algum número para continuar')
+    }else{
+        for(var c = 1;c <= 10; c++ ){
+            restab = c * number 
+            
+            res.innerHTML += (`${number} X ${c} = ${restab}`) 
+        }
+    }
 }
